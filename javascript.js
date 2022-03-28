40245a8b8e671c33a39decd47fd04c89e144af7e
NabhahnK <66389410+NabhahnK@users.noreply.github.com>
2022-03-27 18:14:02 -0700

    make javascript code

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function growBox() {
+   var element = document.getElementById("box");
+   element.style.height = "200px";
+  }
+
+function blueBox() {
+   var element = document.getElementById("box");
+   element.style.backgroundColor = "blue";
+  }
+
+function fadeBox() {
+   var element = document.getElementById("box");
+   element.style.opacity = "0";
+  }
+
+function resetBox() {
+   var element = document.getElementById("box");
+   element.style.height = "150px";
+   element.style.backgroundColor = "orange";
+   element.style.opacity = "1";
+  }
